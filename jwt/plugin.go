ae8cfd2dfdee1dc6e593606cb2dd26ec8d882c85
梵高哦 <1151499519@qq.com>
2023-07-11 14:25:27 +0800

    feat:修改jwt过期配置字段

--- jwt/plugin.go
+++ jwt/plugin.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gly-hub/go-dandelion/database/redigo"
 	"github.com/gomodule/redigo/redis"
 	"github.com/patrickmn/go-cache"
+	"github.com/spf13/cast"
 	"strings"
 	"time"
 )
@@ -28,7 +29,7 @@ type config struct {
 type jwtConfig struct {
 	Model      string `json:"model"`
 	Key        string `json:"key" yaml:"key"`
-	ExpireTime int64  `json:"expire_time" yaml:"expireTime"`
+	ExpireTime string `json:"expire_time" yaml:"expireTime"`
 }
 
 func Plug() *Plugin {
@@ -48,16 +49,25 @@ func (p *Plugin) InitPlugin() error {
 		Config.Jwt.Key = "jwt-key"
 	}
 
-	if Config.Jwt.ExpireTime == 0 {
-		Config.Jwt.ExpireTime = 60 * 60 * 24 * 7
+	var expireTime int64
+	if Config.Jwt.ExpireTime != "" {
+		Config.Jwt.ExpireTime = strings.ReplaceAll(Config.Jwt.ExpireTime, " ", "")
+		numList := strings.Split(Config.Jwt.ExpireTime, "")
+		for _, num := range numList {
+			expireTime = expireTime * cast.ToInt64(num)
+		}
 	}
 
 	if strings.ToLower(Config.Jwt.Model) == "refresh" {
-		Config.Jwt.ExpireTime = 60 * 60
+		expireTime = 60 * 60
 	}
 
-	jwt = NewJwt(Config.Jwt.Key, time.Duration(Config.Jwt.ExpireTime)*time.Second)
-	storage = InitStorage()
+	if expireTime == 0 {
+		expireTime = 60 * 60 * 24 * 7
+	}
+
+	jwt = NewJwt(Config.Jwt.Key, time.Duration(expireTime)*time.Second)
+	storage = InitStorage(expireTime)
 	return nil
 }
 
@@ -122,13 +132,14 @@ func Del(metadata string) error {
 }
 
 type Storage struct {
-	s interface{}
+	s          interface{}
+	expireTime int64
 }
 
 func (s *Storage) Set(key string, value string) (err error) {
 	switch s.s.(type) {
 	case *cache.Cache:
-		s.s.(*cache.Cache).Set(key, value, time.Duration(Config.Jwt.ExpireTime)*time.Second)
+		s.s.(*cache.Cache).Set(key, value, time.Duration(s.expireTime)*time.Second)
 	case *redigo.Client:
 		_, err = s.s.(*redigo.Client).Execute(func(c redis.Conn) (res interface{}, err error) {
 			return c.Do("set", key, value, "EX", Config.Jwt.ExpireTime)
@@ -170,11 +181,11 @@ func (s *Storage) Del(key string) (err error) {
 	return
 }
 
-func InitStorage() *Storage {
+func InitStorage(expireTime int64) *Storage {
 	r := application.Redis{}
 	if db := r.GetRedis(); db != nil {
 		return &Storage{s: db}
 	}
 
-	return &Storage{s: cache.New(5*time.Minute, 10*time.Minute)}
+	return &Storage{s: cache.New(5*time.Minute, 10*time.Minute), expireTime: expireTime}
 }
